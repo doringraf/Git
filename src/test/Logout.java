2e156196eb5cf613e199f92ed3c05e97c425fe95
doringraf <doringraf@gmail.com>
2020-02-29 11:34:01 -0500

    updated logout

--- src/test/Logout.java
+++ src/test/Logout.java
@@ -2,6 +2,9 @@ package test;
 
 public class Logout {
     public static void main(String[] args) {
+
         System.out.println("Logout page ");
+
+        System.out.println("Some updates");
     }
 }
