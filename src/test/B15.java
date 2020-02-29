2f573d6578fe661ec7c8826008e4d279c3cba1e7
doringraf <doringraf@gmail.com>
2020-02-29 11:57:18 -0500

    new update

--- src/test/B15.java
+++ src/test/B15.java
@@ -1,6 +1,12 @@
-/// Helloo
+package test;
+
+public class B15 {
+    public static void main(String[] args) {
+
+        System.out.println("This code has been pulled from GitHUB");
 
 
 
+    }
+}
 
-// This message is from GitHub
--- src/test/Logout.java
+++ src/test/Logout.java
@@ -1,10 +1,17 @@
 package test;
 
 public class Logout {
+   static int number;
+
+    public static void print(){
+        System.out.println(++number);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Logout page ");
 
-        System.out.println("Some updates");
+       print();
     }
 }
+
